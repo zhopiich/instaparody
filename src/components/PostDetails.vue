213c68e2bbc1cef5ec7e991915fa1a3a8054d686
zhopiich <zhoping9449@gmail.com>
2024-05-30 23:30:40 +0800

    feat: new layout of PostDetails modal for mobile devices

--- src/components/PostDetails.vue
+++ src/components/PostDetails.vue
@@ -8,31 +8,53 @@
       class="flex justify-center items-start min-w-0 min-h-0 overflow-y-auto"
     >
       <div class="flex flex-col w-full h-full justify-center items-center my-0">
-        <div class="detailsContainer m-auto h-full">
+        <div
+          class="detailsContainer m-auto h-full"
+          :class="{ mobile: isMobile }"
+        >
           <div class="flex flex-col h-full max-w-full">
             <div
               class="flex justify-center items-stretch relative *:pointer-events-auto"
+              :class="{ 'flex-col': isMobile }"
             >
               <div
-                class="imageFrame aspect-square grow shrink overflow-hidden rounded-l-md"
+                class="imageFrame shrink overflow-hidden"
+                :class="
+                  isMobile
+                    ? 'mobile rounded-t-md'
+                    : 'grow aspect-square rounded-l-md'
+                "
               >
-                <div class="w-full h-full relative bg-black">
-                  <img
-                    v-if="post.image || post.images.length === 1"
-                    class="w-full h-full object-cover"
-                    :src="post.image || post.images[0]"
-                    alt="Image posted"
-                  />
-                  <ImageCarousel v-else :imagesUrl="postImages" />
+                <div :class="isMobile ? 'aspect-square' : 'size-full'">
+                  <div class="size-full relative bg-black">
+                    <img
+                      v-if="post.image || post.images.length === 1"
+                      class="size-full object-cover"
+                      :src="post.image || post.images[0]"
+                      alt="Image posted"
+                    />
+                    <ImageCarousel v-else :imagesUrl="postImages" />
+                  </div>
                 </div>
               </div>
 
               <div
-                class="min-w-[405px] grow shrink-[2] relative rounded-r-md overflow-hidden border-l"
-                :style="{ maxWidth: variableWidth, height: variableHeight }"
+                class="postInfo grow shrink-[2] relative overflow-hidden"
+                :class="
+                  isMobile
+                    ? 'rounded-b-md'
+                    : 'rounded-r-md border-l min-w-[405px]'
+                "
+                :style="{
+                  maxWidth: variableWidth,
+                  height: variableHeight,
+                }"
               >
                 <div class="w-full h-full bg-white flex flex-col">
-                  <div class="grow-0 px-4 py-2.5 flex">
+                  <div
+                    class="grow-0 px-4 flex"
+                    :class="isMobile ? 'pt-2.5' : 'py-2.5'"
+                  >
                     <UserPlate
                       :isCardFixed="true"
                       :user="{
@@ -46,6 +68,7 @@
                     />
                   </div>
                   <div
+                    v-if="!isMobile"
                     class="flex-auto border-t overflow-auto scrollbar-hidden"
                   >
                     <div class="px-4">
@@ -59,8 +82,15 @@
                     </div>
                   </div>
 
-                  <div v-if="!isLikedOrSaved" class="shrink-0 border-t">
-                    <div class="px-4 py-2 flex justify-start">
+                  <div
+                    v-if="!isLikedOrSaved"
+                    class="shrink-0"
+                    :class="{ 'border-t': !isMobile }"
+                  >
+                    <div
+                      class="px-4 flex justify-start"
+                      :class="isMobile ? 'py-0' : 'py-2'"
+                    >
                       <div class="-ml-2">
                         <LikeButton :post="post" />
                       </div>
@@ -75,12 +105,47 @@
                       </div>
                     </div>
 
-                    <div class="px-4 pb-1">
+                    <div class="px-4" :class="isMobile ? 'pb-0' : 'pb-1'">
                       <LikesCountBanner :post="post" />
                     </div>
 
-                    <div class="px-4 pb-4">
+                    <div v-if="isMobile" class="px-4">
+                      <div
+                        class="mt-1 *:text-gray-500 *:leading-4 *:cursor-pointer *:text-sm flex items-center"
+                      >
+                        <span v-if="post.comments === 0" @click="">
+                          Make the first comment
+                        </span>
+                        <span v-else @click="">
+                          {{
+                            "View " +
+                            `${
+                              post.comments === 1
+                                ? "the "
+                                : post.comments === 2
+                                ? "both "
+                                : "all " + post.comments + " "
+                            }` +
+                            "comment" +
+                            `${post.comments > 1 ? "s" : ""}`
+                          }}
+                        </span>
+                      </div>
+                    </div>
+
+                    <div
+                      class="px-4 leading-4"
+                      :class="isMobile ? 'pb-0' : 'pb-4'"
+                    >
                       <TimeBanner :timestamp="post.createdAt?.seconds" />
+                    </div>
+                  </div>
+
+                  <div v-if="isMobile" class="overflow-auto grow">
+                    <div class="px-4 pb-1">
+                      <pre class="font-sans break-words whitespace-pre-wrap">{{
+                        post.description
+                      }}</pre>
                     </div>
                   </div>
 
@@ -119,6 +184,10 @@ import TimeBanner from "./PostButtons/TimeBanner.vue";
 
 import { ref, computed, onMounted, onBeforeUnmount } from "vue";
 import { onBeforeRouteUpdate, onBeforeRouteLeave } from "vue-router";
+
+import { useMediaQueryStore } from "../stores/mediaQuery";
+const mediaQueryStore = useMediaQueryStore();
+const isMobile = computed(() => mediaQueryStore.isMobile);
 
 import { usePostStore } from "../stores/post";
 
@@ -178,6 +247,8 @@ const variableWidth = computed(() => {
   const squareImageWidth = remainedY.value;
   const remainedWidth = remainedX.value - squareImageWidth;
 
+  if (isMobile.value) return "100%";
+
   return (
     (remainedWidth <= 405 ? 405 : remainedWidth <= 500 ? remainedWidth : 500) +
     "px"
@@ -186,6 +257,8 @@ const variableWidth = computed(() => {
 const variableHeight = computed(() => {
   const squareImageWidth = remainedX.value - 405;
   const squareImageHeight = remainedY.value;
+
+  if (isMobile.value) return "265px";
 
   return (
     (squareImageWidth <= 450 || squareImageHeight <= 450
@@ -218,6 +291,10 @@ onBeforeUnmount(() => {
   max-height: calc(100dvh - 40px);
   max-width: calc(100% - calc(2 * 64px));
   width: 100%;
+
+  &.mobile {
+    max-width: calc(100% - calc(2 * 8px));
+  }
 }
 
 .imageFrame {
@@ -226,6 +303,14 @@ onBeforeUnmount(() => {
   max-width: var(--variable);
   flex-basis: var(--variable);
   min-height: 450px;
+
+  &.mobile {
+    --variable-mobile: calc(100dvw - 8px);
+    max-height: var(--variable-mobile);
+    max-width: var(--variable-mobile);
+    flex-basis: auto;
+    min-height: 0;
+  }
 }
 
 .scrollbar-hidden::-webkit-scrollbar {
