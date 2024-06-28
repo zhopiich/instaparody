b275f07f18e37bd29f4e288072d3f38b127b6fc6
zhopiich <zhoping9449@gmail.com>
2024-06-28 17:43:30 +0800

    feat: make the preview image the same width as the images in the bubbles

--- src/components/Messages/ImagePreview.vue
+++ src/components/Messages/ImagePreview.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="max-h-[200px] border-t">
-    <div class="h-full flex justify-end bg-slate-200 relative padding-right">
-      <div class="absolute top-0 left-0 mx-4 my-2">
+  <div class="padding-right bg-slate-200">
+    <div class="px-4 flex items-center">
+      <div class="mr-1">
         <div
           class="flex items-center justify-center h-9 aspect-square rounded-full cursor-pointer hover:bg-red-300/50 transition-colors"
           @click="emit('cancel')"
@@ -10,11 +10,30 @@
         </div>
       </div>
 
-      <div class="h-full preview-bubble overflow-hidden">
-        <img
-          :src="imagePreview"
-          class="max-h-full aspect-square object-cover"
-        />
+      <div class="grow">
+        <div
+          class="preview-bubble overflow-hidden w-full aspect-square cursor-pointer relative"
+          @click="messageStore.openImageViewer(imagePreview)"
+        >
+          <img
+            :src="imagePreview"
+            class="size-full object-cover"
+            alt="image to be sent"
+          />
+
+          <div
+            class="magnifyingGlass absolute right-0 bottom-0 h-10 aspect-square rounded-full overflow-hidden backdrop-blur m-3 transition-transform duration-200"
+          >
+            <div
+              class="size-full flex justify-center items-center bg-white/25 hover:bg-white/65 *:hover:text-black active:bg-white/75 *:active:text-neutral-500 transition-colors"
+            >
+              <FontAwesomeIcon
+                :icon="faMagnifyingGlassPlus"
+                class="fa-xl text-neutral-700"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -22,7 +41,10 @@
 
 <script setup>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrashCan,
+  faMagnifyingGlassPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { ref, shallowRef, computed, watch, onMounted } from "vue";
 
@@ -36,7 +58,7 @@ const emit = defineEmits(["cancel"]);
 
 <style scoped>
 .padding-right {
-  padding-right: calc(v-bind(scrollbarWidth) + 16px);
+  padding-right: calc(v-bind(scrollbarWidth));
 }
 
 .preview-bubble {
@@ -44,5 +66,13 @@ const emit = defineEmits(["cancel"]);
   border-top-right-radius: 1.5rem;
   border-bottom-left-radius: 1.5rem;
   border-bottom-right-radius: 0.25rem;
+}
+
+.magnifyingGlass {
+  transform: translateY(calc(100% + 12px));
+}
+
+.preview-bubble:hover .magnifyingGlass {
+  transform: translateY(0);
 }
 </style>
