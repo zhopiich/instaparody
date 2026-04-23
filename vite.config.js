76f92ce916cd326a72f1086f31498bbd745bec33
zhopiich <zhoping9449@gmail.com>
2026-04-23 18:53:07 +0800

    build(vite): remove deprecated vendor chunk plugin references

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {
   defineConfig,
-  //  splitVendorChunkPlugin
 } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -8,7 +7,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [
     vue(),
-    //  splitVendorChunkPlugin()
   ],
   // server: {
   //   proxy: {
