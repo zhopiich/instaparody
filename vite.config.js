7d1cb5e3ad521ce5b249c17b891b95bf6de8c541
zhopiich <zhoping9449@gmail.com>
2026-04-23 19:01:50 +0800

    build(deps): upgrade to Vite 8 and migrate to Rolldown

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   //   },
   // },
   build: {
-    rollupOptions: {
+    rolldownOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
